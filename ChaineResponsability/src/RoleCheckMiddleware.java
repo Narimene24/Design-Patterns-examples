8c22f4afb83edcc6415ad3098c789ce0c055c3e4
narimene TEMAL <36580268+Narimene24@users.noreply.github.com>
2023-12-16 02:38:26 +0100

    Chaine Of Responsability Design Patter

---
+++ ChaineResponsability/src/RoleCheckMiddleware.java
@@ -0,0 +1,19 @@
+
+
+
+//  middleware/RoleCheckMiddleware.java: Vérifier le rôle de l’utilisateur
+
+
+/**
+ * ConcreteHandler. Checks a user's role.
+ */
+public class RoleCheckMiddleware extends Middleware {
+    public boolean check(String email, String password) {
+        if (email.equals("admin@example.com")) {
+            System.out.println("Hello, admin!");
+            return true;
+        }
+        System.out.println("Hello, user!");
+        return checkNext(email, password);
+    }
+}
