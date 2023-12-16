c0a1ab28de057bc8ca359ce159811e6e1b3d97a1
narimene TEMAL <36580268+Narimene24@users.noreply.github.com>
2023-12-16 02:31:38 +0100

    Template Design Pattern (Fonction)

---
+++ TemplatePattern/src/App.java
@@ -0,0 +1,16 @@
+
+
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();		
+     }
+     
+}
