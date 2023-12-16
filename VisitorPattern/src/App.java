2afda1b0eb05181bba42da3bd023eb11817bea72
narimene TEMAL <36580268+Narimene24@users.noreply.github.com>
2023-12-16 02:30:46 +0100

    Visitor Design Pattern

---
+++ VisitorPattern/src/App.java
@@ -0,0 +1,13 @@
+
+
+
+
+
+public class App {
+    public static void main(String[] args) {
+
+        ComputerPart computer = new Computer();
+        computer.accept(new ComputerPartDisplayVisitor());
+        
+     }
+}
